09a9a92ae98779b8eb1df43d2b569090ce04c1e8
raghul <raghulramakrishnan2004@gmail.com>
2025-02-18 11:24:00 +0530

    ch-7

--- Mekchat25.0/src/components/MechRegiser.jsx
+++ Mekchat25.0/src/components/MechRegiser.jsx
@@ -15,7 +15,7 @@ const MechRegiser = () => {
             <p>1.For only Tech Events, the entry fee will be ₹250.</p>
             <p>2.For both Tech and Non-Tech Events, the entry fee will be ₹300.</p>
             <a to="/register" id="neon-btn" href="https://docs.google.com/forms/d/e/1FAIpQLSe2pJ-8mqd92tvxI4Dv_883YsJ38sFp7gvHqko7W4XMFmJUzg/viewform" target="_blank">
-                    <button className="custom-btn1 btn-12">Register</button>
+                    <button className="mr-custom-btn1 mr-btn-12"><span>Events</span></button>
             </a>
         </div>
         <div className="mr-workshop">
@@ -23,10 +23,16 @@ const MechRegiser = () => {
             <p>Industry 4.O - ₹390</p>
             <p>Solidworks - ₹350</p>
             <p>Both Industry 4.O & Solidworks - ₹550</p>
+            <a to="/register" id="neon-btn" href="https://docs.google.com/forms/d/e/1FAIpQLSe2pJ-8mqd92tvxI4Dv_883YsJ38sFp7gvHqko7W4XMFmJUzg/viewform" target="_blank">
+                    <button className="mr-custom-btn1 mr-btn-12"><span>Workshop</span></button>
+            </a>
         </div>
         <div className="mr-ewaste">
             <h1>🚀E-waste</h1>
             <p>The Enrollment fee for e-waste management is ₹200</p>
+            <a to="/register" id="neon-btn" href="https://docs.google.com/forms/d/e/1FAIpQLSe2pJ-8mqd92tvxI4Dv_883YsJ38sFp7gvHqko7W4XMFmJUzg/viewform" target="_blank">
+                    <button className="mr-custom-btn1 mr-btn-12"><span>E-waste</span></button>
+            </a>
         </div>
         </div>
     </div>
