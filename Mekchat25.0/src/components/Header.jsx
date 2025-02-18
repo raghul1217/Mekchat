0c47481ad6d0f3c4b306d7effb486a51e006ab87
raghul <raghulramakrishnan2004@gmail.com>
2025-02-18 13:29:16 +0530

    ch-14

--- Mekchat25.0/src/components/Header.jsx
+++ Mekchat25.0/src/components/Header.jsx
@@ -43,27 +43,61 @@ function Header() {
                     <div className="menu-icon" onClick={toggleMenu}>
                         {menuOpen ? '\u2715' : '\u2630'}
                     </div>
-                    <ul className={`nav-links ${menuOpen ? 'active' : ''}`}>
-                        <li><a href="#home" onClick={() => handleLinkClick('/')}>Home</a></li>
-                        <li><a href="#events" onClick={() => handleLinkClick('/#events')}>Events</a></li>
-                        <li><a href="#profile" onClick={() => handleLinkClick('/profile')}>Profile</a></li>
-                        <li><a href="#register" onClick={() => handleLinkClick('#')}>Register</a></li>
-                        <li><a href="#payment" onClick={() => handleLinkClick('#')}>Payment</a></li>
-                        <li><a href="#footer" onClick={() => handleLinkClick('/#footer')}>Contact</a></li>
+                    <ul className={`nav-links ${menuOpen ? "active" : ""}`}>
+            <li>
+              <a href="#home" onClick={() => handleLinkClick("/#home")}>
+                Home
+              </a>
+            </li>
+            <li>
+              <a href="#clubs" onClick={() => handleLinkClick("/#clubs")}>
+                Clubs
+              </a>
+            </li>
+            <li>
+              <a href="#events" onClick={() => handleLinkClick("/#events")}>
+                Events
+              </a>
+            </li>
+            
+            {/* <li>
+              <a href="#profile" onClick={() => handleLinkClick("/profile")}>
+                Profile
+              </a>
+            </li>
+            <li>
+              <a href="#register" onClick={() => handleLinkClick("/register")}>
+                Register
+              </a>
+            </li>
+            <li> */}
+              {/* <a href="#payment" onClick={() => handleLinkClick("/payment")}>
+                Payment
+              </a>
+            </li> */}
+            <li>
+              <a href="#footer" onClick={() => handleLinkClick("/#footer")}>
+                Contact
+              </a>
+            </li>
 
-                        {/* Conditionally render Login or Logout button */}
-                        {isLoggedIn ? (
-                            <li>
-                                <button className="custom-btn1 btn-12" onClick={handleLogout}>
-                                    <span className='logout-span'>Logout</span>
-                                </button>
-                            </li>
-                        ) : (
-                            <button className="custom-btn1 btn-12" onClick={() => handleLinkClick('/login')}>
-                                    <span className='logout-span'>Login</span>
-                            </button>
-                        )}
-                    </ul>
+            {/* Conditionally render Login or Logout button */}
+            {isLoggedIn ? (
+              <li>
+                <button className="custom-btn btn-12" onClick={handleLogout}>
+                  <span className="logout-span" id="header-btn">
+                    Logout
+                  </span>
+                </button>
+              </li>
+            ) : (
+              <button className="custom-btn btn-12" onClick={() => window.open("https://drive.google.com/drive/folders/10rg6bsXhj8f7FAUODSHTb_Nil_ho9SYz", "_blank")}>
+                <span className="logout-span" id="header-btn">
+                  <i className="fas fa-download"></i> Rulebook
+                </span>
+              </button>
+            )}
+          </ul>
                 </nav>
             </div>
         </header>
